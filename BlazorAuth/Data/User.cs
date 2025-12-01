405505a68dafbe126352a2fd7cad7f1d82c598f7
tahoad <nowwatchitclosely@gmail.com>
2025-12-02 02:57:59 +0700

    first commit

---
+++ BlazorAuth/Data/User.cs
@@ -0,0 +1,30 @@
+﻿namespace BlazorAuth.Data
+{
+    public class User
+    {
+        public int Id { get; set; }
+        public string user_name { get; set; } = string.Empty;
+        public string password { get; set; } = string.Empty;
+        public string role { get; set; } = string.Empty;
+
+        public List<User> GetUser()
+        {
+            return new List<User> {
+                new User
+                {
+                    Id = 1,
+                    user_name = "admin",
+                    password = "admin123",
+                    role = "Administrator",
+                },
+                new User
+                {
+                     Id = 2,
+                    user_name = "user",
+                    password = "user123",
+                    role = "User",
+                }
+            };
+        }
+    }
+}
---
+++ BlazorAuth/Models/ViewModels/LoginViewModel.cs
@@ -0,0 +1,12 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace BlazorAuth.Models.ViewModels
+{
+    public class LoginViewModel
+    {
+        [Required(ErrorMessage = "กรุณาใส่ UserName")]
+        public string? UserName { get; set; }
+        [Required(ErrorMessage = "กรุณาใส่ Password")]
+        public string? Password { get; set; }
+    }
+}
